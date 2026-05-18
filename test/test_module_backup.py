afeb0170f2a5b49b73d739374e30a0ec8bb0e9f7
anpeihang.1 <anpeihang.1@jd.com>
2026-05-18 09:36:40 +0800

    test backup test in cicd

--- test/test_module_backup.py
+++ test/test_module_backup.py
@@ -310,6 +310,7 @@ class TestBackup:
 
         space_config = {
             "name": space_name,
+            # partition_num 1 in there
             "partition_num": 1,
             "replica_num": 1,
             "fields": properties["fields"]
